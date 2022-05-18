67569a72ca1c347373b17b950b444b5d10b68baf
shk <kim141312@naver.com>
2022-05-19 02:26:11 +0900

    Quiz Output ver.0.85
    Quiz and hint output

---
+++ app/src/main/java/com/example/teamns_arcore/game/HintDialog.java
@@ -0,0 +1,28 @@
+package com.example.teamns_arcore.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.teamns_arcore.R;
+
+public class HintDialog extends AppCompatActivity {
+
+//    TextView hintTxtView;
+//    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hint_dialog);
+
+//        hintTxtView = findViewById(R.id.hintTxtView);
+
+//        Intent intent = getIntent();
+//        String[] ranNum = intent.getStringArrayExtra("RandomEng");
+
+//        hintTxtView.setText("힌트 바뀜");
+    }
+}
